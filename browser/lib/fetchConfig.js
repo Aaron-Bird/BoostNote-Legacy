cde2e27e049136519fb05c6e3791a5ae48dfb04d
Rokt33r <fluke8259@gmail.com>
2016-01-19 18:47:10 +0900

    must override default config

--- browser/lib/fetchConfig.js
+++ browser/lib/fetchConfig.js
@@ -5,6 +5,17 @@ const jetpack = require('fs-jetpack')
 const userDataPath = remote.app.getPath('userData')
 const configFile = 'config.json'
 
+const defaultConfig = {
+  'editor-font-size': '14',
+  'editor-font-family': 'Monaco, Consolas',
+  'editor-indent-type': 'space',
+  'editor-indent-size': '4',
+  'preview-font-size': '14',
+  'preview-font-family': 'Lato',
+  'switch-preview': 'blur',
+  'disable-direct-write': false
+}
+
 export default function fetchConfig () {
-  return Object.assign({}, JSON.parse(jetpack.cwd(userDataPath).read(configFile, 'utf-8')))
+  return Object.assign({}, defaultConfig, JSON.parse(jetpack.cwd(userDataPath).read(configFile, 'utf-8')))
 }
