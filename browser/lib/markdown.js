01c9d62a2b44554ab8ecc139992730f48e0419d9
Rokt33r <fluke8259@gmail.com>
2016-01-21 05:50:40 +0900

    fix sanitizing bug in Code block

--- browser/lib/markdown.js
+++ browser/lib/markdown.js
@@ -14,7 +14,7 @@ var md = markdownit({
         return hljs.highlight(lang, str).value
       } catch (e) {}
     }
-    return str
+    return str.replace(/\&/g, '&amp;').replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace(/\"/g, '&quot;')
   }
 })
 md.use(emoji, {
