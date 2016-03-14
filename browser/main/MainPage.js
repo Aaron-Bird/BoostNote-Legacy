593d242a4c38cb3c9b7c945235a7efd42ef8ecc7
Rokt33r <fluke8259@gmail.com>
2016-03-15 02:24:28 +0900

    ctrl +  wheel to change zoom factor

--- browser/main/MainPage.js
+++ browser/main/MainPage.js
@@ -19,9 +19,27 @@ export default class MainContainer extends React.Component {
     ipc.send('update-app', 'Deal with it.')
   }
 
+  handleWheel (e) {
+    if (e.ctrlKey && process.platform !== 'darwin') {
+      if (window.document.body.style.zoom == null) {
+        window.document.body.style.zoom = 1
+      }
+      console.log(window.document.body.style.zoom)
+      let zoom = Number(window.document.body.style.zoom)
+      if (e.deltaY > 0 && zoom < 4) {
+        document.body.style.zoom = zoom + 0.05
+      } else if (e.deltaY < 0 && zoom > 0.5) {
+        document.body.style.zoom = zoom - 0.05
+      }
+    }
+  }
+
   render () {
     return (
-      <div className='Main'>
+      <div
+        className='Main'
+        onWheel={(e) => this.handleWheel(e)}
+      >
         {this.state.updateAvailable ? (
         <button onClick={this.updateApp} className='appUpdateButton'><i className='fa fa-cloud-download'/> Update available!</button>
         ) : null}
