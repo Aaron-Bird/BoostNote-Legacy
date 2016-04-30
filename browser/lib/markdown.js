084677cdca94ef4069b9cb231aa049c906fd4d88
Egon schmid <eschmid1972@gmail.com>
2016-04-30 21:30:52 +0200

    code style

--- browser/lib/markdown.js
+++ browser/lib/markdown.js
@@ -6,8 +6,8 @@ import hljs from 'highlight.js'
 var createGutter = function (str) {
   var lc = (str.match(/\n/g) || []).length;
   var lines = [];
-  for (var i=1; i <= lc; i++) {
-    lines.push('<span>'+i+'</span>');
+  for (var i = 1; i <= lc; i++) {
+    lines.push('<span>' + i + '</span>');
   }
   return '<span>' + lines.join('') + '</span>';
 };
