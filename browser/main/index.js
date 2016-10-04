5f240ada590bcbf6108f4145cb7574b09a858347
Dick Choi <fluke8259@gmail.com>
2016-10-04 11:57:12 +0900

    apply devtron

--- browser/main/index.js
+++ browser/main/index.js
@@ -25,6 +25,10 @@ document.addEventListener('dragover', function (e) {
   e.stopPropagation()
 })
 
+if (process.env !== 'production') {
+  require('devtron').install()
+}
+
 let el = document.getElementById('content')
 const history = syncHistoryWithStore(hashHistory, store)
 
