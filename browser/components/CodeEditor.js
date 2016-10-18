8f1c198406d68ef7818a84f4201c6df446e14592
Dick Choi <fluke8259@gmail.com>
2016-10-18 13:42:10 +0900

    hot fix: Wrong behavior with Japanese IME keyboard

--- browser/components/CodeEditor.js
+++ browser/components/CodeEditor.js
@@ -50,7 +50,7 @@ export default class CodeEditor extends React.Component {
       tabSize: this.props.indentSize,
       indentWithTabs: this.props.indentType !== 'space',
       keyMap: 'sublime',
-      inputStyle: 'contenteditable',
+      inputStyle: 'textarea',
       extraKeys: {
         Tab: function (cm) {
           if (cm.somethingSelected()) cm.indentSelection('add')
