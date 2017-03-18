806c3bbaf97ec5c08c71147d56ab87600751ed5e
Sosuke Suzuki <aosukeke@gmail.com>
2017-03-18 12:27:43 +0900

    delete unnecessary `}` and correct the indent

--- browser/main/Detail/MarkdownNoteDetail.js
+++ browser/main/Detail/MarkdownNoteDetail.js
@@ -69,12 +69,11 @@ class MarkdownNoteDetail extends React.Component {
 
     for (let i = 0; i < splitted.length; i++) {
       let trimmedLine = splitted[i].trim()
-      if (trimmedLine.match('```')){
+      if (trimmedLine.match('```')) {
         isMarkdownInCode = !isMarkdownInCode
       } else if (isMarkdownInCode === false && trimmedLine.match(/^# +/)) {
-          title = trimmedLine.substring(1, trimmedLine.length).trim()
-          break
-        }
+        title = trimmedLine.substring(1, trimmedLine.length).trim()
+        break
       }
     }
 
