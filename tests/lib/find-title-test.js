2bbe7056d180b3601a6c96f703a3e146982f0904
asmsuechan <suenagaryoutaabc@gmail.com>
2017-05-23 15:43:54 +0900

    Add test cases

--- tests/lib/find-title-test.js
+++ tests/lib/find-title-test.js
@@ -9,9 +9,11 @@ const { findNoteTitle } = require('browser/lib/findNoteTitle')
 test('findNoteTitle#find  should return a correct title (string)', t => {
   // [input, expected]
   const testCases = [
-    ['# hoge\nhoge', 'hoge'],
+    ['# hoge\nfuga', 'hoge'],
     ['# hoge_hoge_hoge', 'hoge_hoge_hoge'],
-    ['```\n# hoge\n```\n# fuga', 'fuga']
+    ['```\n# hoge\n```\n# fuga', 'fuga'],
+    ['```\n# hoge\n```', '```'],
+    ['hoge', 'hoge'],
   ]
 
   testCases.forEach(testCase => {
