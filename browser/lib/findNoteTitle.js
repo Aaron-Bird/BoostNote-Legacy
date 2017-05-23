908cd7a890cfbc8badfc081f5f206dea14df9e3c
asmsuechan <suenagaryoutaabc@gmail.com>
2017-05-23 15:27:22 +0900

    Edit the method

--- browser/lib/findNoteTitle.js
+++ browser/lib/findNoteTitle.js
@@ -3,24 +3,31 @@ export function findNoteTitle (value) {
   let title = null
   let isMarkdownInCode = false
 
-  for (let i = 0; i < splitted.length; i++) {
-    let trimmedLine = splitted[i].trim()
+  splitted.some((line, index) => {
+    let trimmedLine = line.trim()
+    let trimmedNextLine = splitted[index + 1] === undefined ? '' : splitted[index + 1].trim()
     if (trimmedLine.match('```')) {
       isMarkdownInCode = !isMarkdownInCode
-    } else if (isMarkdownInCode === false && trimmedLine.match(/^# +/)) {
-      title = trimmedLine.substring(1, trimmedLine.length).trim()
-      break
+    } else if (isMarkdownInCode === false && (trimmedLine.match(/^# +/) || trimmedNextLine.match('='))) {
+      if (trimmedNextLine.match('=')) {
+        title = trimmedLine.substring(0, trimmedLine.length).trim()
+      } else {
+        title = trimmedLine.substring(1, trimmedLine.length).trim()
+      }
+      return true
     }
-  }
+  })
 
-  if (title === null) {
-    title = ''
+  if (title == null) {
     for (let i = 0; i < splitted.length; i++) {
       let trimmedLine = splitted[i].trim()
       if (trimmedLine.length > 0) {
         title = trimmedLine
         break
       }
+    }
+    if (title == null) {
+      title = ''
     }
   }
 
