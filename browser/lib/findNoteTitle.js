99228f2e60f41ac29ea788696e6fcbc7d1db4b22
asmsuechan <suenagaryoutaabc@gmail.com>
2017-05-23 16:51:58 +0900

    :recycle: Refactor findeTitle()

--- browser/lib/findNoteTitle.js
+++ browser/lib/findNoteTitle.js
@@ -1,29 +1,25 @@
 export function findNoteTitle (value) {
   let splitted = value.split('\n')
   let title = null
-  let isMarkdownInCode = false
+  let isInsideCodeBlock = false
 
   splitted.some((line, index) => {
     let trimmedLine = line.trim()
     let trimmedNextLine = splitted[index + 1] === undefined ? '' : splitted[index + 1].trim()
     if (trimmedLine.match('```')) {
-      isMarkdownInCode = !isMarkdownInCode
-    } else if (isMarkdownInCode === false && (trimmedLine.match(/^# +/) || trimmedNextLine.match('='))) {
-      if (trimmedNextLine.match('=')) {
-        title = trimmedLine.substring(0, trimmedLine.length).trim()
-      } else {
-        title = trimmedLine.substring(1, trimmedLine.length).trim()
-      }
+      isInsideCodeBlock = !isInsideCodeBlock
+    }
+    if (isInsideCodeBlock === false && (trimmedLine.match(/^# +/) || trimmedNextLine.match(/^=+$/))) {
+      title = trimmedLine
       return true
     }
   })
 
-  if (title == null) {
+  if (title === null) {
     title = ''
-    splitted.some((line, index) => {
-      let trimmedLine = splitted[index].trim()
-      if (trimmedLine.length > 0) {
-        title = trimmedLine
+    splitted.some((line) => {
+      if (line.trim().length > 0) {
+        title = line.trim()
         return true
       }
     })
--- tests/lib/find-title-test.js
+++ tests/lib/find-title-test.js
@@ -9,9 +9,10 @@ const { findNoteTitle } = require('browser/lib/findNoteTitle')
 test('findNoteTitle#find  should return a correct title (string)', t => {
   // [input, expected]
   const testCases = [
-    ['# hoge\nfuga', 'hoge'],
-    ['# hoge_hoge_hoge', 'hoge_hoge_hoge'],
-    ['```\n# hoge\n```\n# fuga', 'fuga'],
+    ['# hoge\nfuga', '# hoge'],
+    ['# hoge_hoge_hoge', '# hoge_hoge_hoge'],
+    ['hoge\n====\nfuga', 'hoge'],
+    ['====', '===='],
     ['```\n# hoge\n```', '```'],
     ['hoge', 'hoge']
   ]
