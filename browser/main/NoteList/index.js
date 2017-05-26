7abf53009ad4e3bb3d76dc8c3328dd7422a01742
Sosuke Suzuki <aosukeke@gmail.com>
2017-05-26 18:01:44 +0900

    change search ux

--- browser/main/NoteList/index.js
+++ browser/main/NoteList/index.js
@@ -212,6 +212,10 @@ class NoteList extends React.Component {
         .map((uniqueKey) => data.noteMap.get(uniqueKey))
     }
 
+    if (location.pathname.match(/\/searched/)) {
+      return this.getSearchNotes()
+    }
+
     let storageKey = params.storageKey
     let folderKey = params.folderKey
     let storage = data.storageMap.get(storageKey)
@@ -347,6 +351,71 @@ class NoteList extends React.Component {
         key: this.notes[targetIndex].storage + '-' + this.notes[targetIndex].key
       }
     })
+  }
+
+  getSearchNotes () {
+    let { data } = this.props
+    let search = document.getElementsByClassName('TopBar__control-search-input___browser-main-TopBar-')[0].childNodes[0].value
+    let notes = data.noteMap.map((note) => note)
+    if (search.trim().length === 0) return []
+    let searchBlocks = search.split(' ')
+    searchBlocks.forEach((block) => {
+      if (block.match(/^!#.+/)) {
+        let tag = block.match(/^!#(.+)/)[1]
+        let regExp = new RegExp(_.escapeRegExp(tag), 'i')
+        notes = notes
+          .filter((note) => {
+            if (!_.isArray(note.tags)) return false
+            return note.tags.some((_tag) => {
+              return _tag.match(regExp)
+            })
+          })
+      } else if (block.match(/^!.+/)) {
+        let block = block.match(/^!(.+)/)[1]
+        let regExp = new RegExp(_.escapeRegExp(block), 'i')
+        notes = notes.filter((note) => {
+          if (!_.isArray(note.tags) || !note.tags.some((_tag) => {
+            return _tag.match(regExp)
+          })) {
+            return true
+          }
+          if (note.type === 'SNIPPET_NOTE') {
+            return !note.description.match(regExp)
+          } else if (note.type === 'MARKDOWN_NOTE') {
+            return !note.content.match(regExp)
+          }
+          return false
+        })
+      } else if (block.match(/^#.+/)) {
+        let tag = block.match(/#(.+)/)[1]
+        let regExp = new RegExp(_.escapeRegExp(tag), 'i')
+        notes = notes
+          .filter((note) => {
+            if (!_.isArray(note.tags)) return false
+            return note.tags.some((_tag) => {
+              return _tag.match(regExp)
+            })
+          })
+      } else {
+        let regExp = new RegExp(_.escapeRegExp(block), 'i')
+        notes = notes.filter((note) => {
+          if (_.isArray(note.tags) && note.tags.some((_tag) => {
+            return _tag.match(regExp)
+          })) {
+            return true
+          }
+          if (note.type === 'SNIPPET_NOTE') {
+            return note.description.match(regExp)
+          } else if (note.type === 'MARKDOWN_NOTE') {
+            return note.content.match(regExp)
+          }
+          return false
+        })
+      }
+  })
+
+    return notes
+
   }
 
   render () {
