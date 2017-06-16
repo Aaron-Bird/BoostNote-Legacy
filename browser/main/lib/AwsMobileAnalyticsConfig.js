f443f9264a87d8b215e395bac7a023ea3db1561b
Kohei TAKATA <kt.koheitakata@gmail.com>
2017-06-16 21:35:21 +0900

    Fix initialize logic of analytics

--- browser/main/lib/AwsMobileAnalyticsConfig.js
+++ browser/main/lib/AwsMobileAnalyticsConfig.js
@@ -2,17 +2,17 @@ const AWS = require('aws-sdk')
 const AMA = require('aws-sdk-mobile-analytics')
 const ConfigManager = require('browser/main/lib/ConfigManager')
 
+AWS.config.region = 'us-east-1'
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-east-1:xxxxxxxxxxxxxxxxxxxxxxxxx'
+})
+
 const mobileAnalyticsClient = new AMA.Manager({
   appId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
   appTitle: 'xxxxxxxxxx'
 })
 
 function initAwsMobileAnalytics () {
-  AWS.config.region = 'us-east-1'
-  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:xxxxxxxxxxxxxxxxxxxxxxxxx'
-  })
-
   AWS.config.credentials.get((err) => {
     if (!err) {
       console.log('Cognito Identity ID: ' + AWS.config.credentials.identityId)
