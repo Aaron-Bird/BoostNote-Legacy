9405b95825ce40b5d9b3f88519e28b2a903605e6
asmsuechan <suenagaryoutaabc@gmail.com>
2017-08-12 09:11:22 +0900

    Change RcParser testable

--- browser/main/lib/RcParser.js
+++ browser/main/lib/RcParser.js
@@ -1,11 +1,11 @@
 import path from 'path'
 import sander from 'sander'
 
-function parse () {
-  const BOOSTNOTERC = '.boostnoterc'
-  const homePath = global.process.env.HOME || global.process.env.USERPROFILE
-  const boostnotercPath = path.join(homePath, BOOSTNOTERC)
+const BOOSTNOTERC = '.boostnoterc'
+const homePath = global.process.env.HOME || global.process.env.USERPROFILE
+const _boostnotercPath = path.join(homePath, BOOSTNOTERC)
 
+function parse (boostnotercPath = _boostnotercPath) {
   if (!sander.existsSync(boostnotercPath)) return {}
   try {
     return JSON.parse(sander.readFileSync(boostnotercPath).toString())
