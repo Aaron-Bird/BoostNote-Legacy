66f3ce2cb290608259e82ab3499eb6dbaa2e052d
Sosuke Suzuki <aosukeke@gmail.com>
2017-09-12 19:43:59 +0900

    add MAIN_FOCUSED event

--- browser/main/lib/AwsMobileAnalyticsConfig.js
+++ browser/main/lib/AwsMobileAnalyticsConfig.js
@@ -18,9 +18,10 @@ function initAwsMobileAnalytics () {
   AWS.config.credentials.get((err) => {
     if (!err) {
       console.log('Cognito Identity ID: ' + AWS.config.credentials.identityId)
+      recordDynamicCustomEvent('MAIN_FOCUSED')
+      recordStaticCustomEvent()
     }
   })
-  recordStaticCustomEvent()
 }
 
 function recordDynamicCustomEvent (type) {
