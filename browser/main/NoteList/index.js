706b5d253f4603cf7aa1567c963285a6b50d7193
Kazu Yokomizo <kaz@maisin.co>
2017-11-06 15:49:04 +0900

    Fix updated date at note list

--- browser/main/NoteList/index.js
+++ browser/main/NoteList/index.js
@@ -488,6 +488,25 @@ class NoteList extends React.Component {
       if (note.isTrashed !== true || location.pathname === '/trashed') return true
     })
 
+  moment.locale('en', {
+    relativeTime: {
+      future: 'in %s',
+      past: '%s ago',
+      s:  '%ds',
+      ss: '%ss',
+      m:  '1m',
+      mm: '%dm',
+      h:  'an hour',
+      hh: '%dh',
+      d:  '1 d',
+      dd: '%dd',
+      M:  'a month',
+      MM: '%dM',
+      y:  'a year',
+      yy: '%dY'
+    }
+  });
+
     let noteList = notes
       .map(note => {
         if (note == null) {
@@ -499,7 +518,7 @@ class NoteList extends React.Component {
         const dateDisplay = moment(
           config.sortBy === 'CREATED_AT'
             ? note.createdAt : note.updatedAt
-        ).fromNow()
+        ).fromNow('D')
         const key = `${note.storage}-${note.key}`
 
         if (isDefault) {
