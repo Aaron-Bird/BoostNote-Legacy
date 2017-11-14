34a16298d4c048234932e7a3a803884d2b0a8f7b
Kazu Yokomizo <kaz@maisin.co>
2017-11-14 21:15:00 +0900

    Change markdown lock icon

--- browser/main/Detail/MarkdownNoteDetail.js
+++ browser/main/Detail/MarkdownNoteDetail.js
@@ -232,7 +232,7 @@ class MarkdownNoteDetail extends React.Component {
   }
 
   getToggleLockButton () {
-    return this.state.isLocked ? 'fa-lock' : 'fa-unlock'
+    return this.state.isLocked ? 'fa-eye-slash' : 'fa-eye'
   }
 
   handleDeleteKeyDown (e) {
@@ -339,9 +339,6 @@ class MarkdownNoteDetail extends React.Component {
               onMouseDown={(e) => this.handleLockButtonMouseDown(e)}
             >
               <i className={faClassName} styleName='lock-button' />
-              <span styleName='control-lockButton-tooltip'>
-                {this.state.isLocked ? 'Unlock Editor' : 'Keep Editor Locked'}
-              </span>
             </button>
           return (
             this.state.isLockButtonShown ? lockButtonComponent : ''
