c397752b7a7f05253bd66152f4e4ced1c2667914
Kazu Yokomizo <kaz@maisin.co>
2017-11-18 14:47:39 +0900

    Fix at InfoButton

--- browser/main/Detail/InfoButton.js
+++ browser/main/Detail/InfoButton.js
@@ -8,9 +8,7 @@ const InfoButton = ({
   <button styleName='control-infoButton'
     onClick={onClick}
   >
-    <img styleName='iconInfo' 
-      src='../resources/icon/icon-info.svg'
-    />
+    <img styleName='iconInfo' src='../resources/icon/icon-info.svg' />
   </button>
 )
 
