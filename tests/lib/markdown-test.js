1af374439d66dfcddfe74c6e8008ea8410e0974b
Yu-Hung Ou <xanatosou@gmail.com>
2018-03-11 23:18:35 +1100

    added unit tests for lib/markdown

---
+++ tests/lib/markdown-test.js
@@ -0,0 +1,22 @@
+import test from 'ava'
+import Markdown from 'browser/lib/markdown'
+import markdownFixtures from '../fixtures/markdowns'
+
+// basic markdown instance which meant to be used in every test cases.
+// To test markdown options, initialize a new instance in your test case
+const md = new Markdown()
+
+test('Markdown.render() should renders markdown correctly', t => {
+  const rendered = md.render(markdownFixtures.basic)
+  t.snapshot(rendered)
+})
+
+test('Markdown.render() should renders codeblock correctly', t => {
+  const rendered = md.render(markdownFixtures.codeblock)
+  t.snapshot(rendered)
+})
+
+test('Markdown.render() should renders KaTeX correctly', t => {
+  const rendered = md.render(markdownFixtures.katex)
+  t.snapshot(rendered)
+})
