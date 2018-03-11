bbcd674516aeeaf1ceaf205c478c56ea1f450b6b
Yu-Hung Ou <xanatosou@gmail.com>
2018-03-11 23:17:14 +1100

    updated lib/consts to access electron remote.app only when in production mode

--- browser/lib/consts.js
+++ browser/lib/consts.js
@@ -1,10 +1,9 @@
 const path = require('path')
 const fs = require('sander')
 const { remote } = require('electron')
-const { app } = remote
 
 const themePath = process.env.NODE_ENV === 'production'
-  ? path.join(app.getAppPath(), './node_modules/codemirror/theme')
+  ? path.join(remote.app.getAppPath(), './node_modules/codemirror/theme')
   : require('path').resolve('./node_modules/codemirror/theme')
 const themes = fs.readdirSync(themePath)
   .map((themePath) => {
