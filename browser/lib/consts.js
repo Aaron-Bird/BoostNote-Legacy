8bf5d02624e9f6b51bc858f502d0603c4f05dd79
Yu-Hung Ou <xanatosou@gmail.com>
2018-03-15 22:12:22 +1100

    Revert "updated lib/consts to access electron remote.app only when in production mode"
    This reverts commit bbcd674516aeeaf1ceaf205c478c56ea1f450b6b.

--- browser/lib/consts.js
+++ browser/lib/consts.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const fs = require('sander')
 const { remote } = require('electron')
+const { app } = remote
 
 const themePath = process.env.NODE_ENV === 'production'
-  ? path.join(remote.app.getAppPath(), './node_modules/codemirror/theme')
+  ? path.join(app.getAppPath(), './node_modules/codemirror/theme')
   : require('path').resolve('./node_modules/codemirror/theme')
 const themes = fs.readdirSync(themePath)
   .map((themePath) => {
