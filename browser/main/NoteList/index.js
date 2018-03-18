842ece2a8a64d1307b284b237dcf8bb0ea230c97
Frank Kanis <frank.kanis@gmail.com>
2018-03-18 11:14:22 +0100

    Fix Copy Note Link in Context Menu

--- browser/main/NoteList/index.js
+++ browser/main/NoteList/index.js
@@ -671,7 +671,7 @@ class NoteList extends React.Component {
   }
 
   copyNoteLink (note) {
-    const noteLink = `[${note.title}](${note.key})`
+    const noteLink = `[${note.title}](:note:${note.key})`
     return copy(noteLink)
   }
 
