ddcd7225987d25f864f34a1e6c876569a208d2e9
Hung Nguyen <viethungax@gmail.com>
2018-04-21 09:07:49 +0700

    fix eslint error

--- browser/lib/consts.js
+++ browser/lib/consts.js
@@ -12,13 +12,13 @@ const themes = fs.readdirSync(themePath)
   })
 themes.splice(themes.indexOf('solarized'), 1, 'solarized dark', 'solarized light')
 
-const snippetFile = process.env.NODE_ENV === 'production' 
+const snippetFile = process.env.NODE_ENV === 'production'
   ? path.join(app.getPath('appData'), 'Boostnote', 'snippets.json')
   : require('path').resolve(path.join(getAppData(), 'Boostnote', 'snippets.json'))
 
 function getAppData () {
-  return process.env.APPDATA || (process.platform == 'darwin' 
-  ? process.env.HOME + 'Library/Preferences' 
+  return process.env.APPDATA || (process.platform === 'darwin'
+  ? process.env.HOME + 'Library/Preferences'
   : '/.config')
 }
 
