32e22dd5070a9f7bed72f78bddf408f3afd74b83
yosmoc <motihara@gmail.com>
2018-04-26 23:46:39 +0200

    update tagNoteMap when delete / restore the notebook
    deleted note should not belong to tagNoteMap

--- browser/main/store.js
+++ browser/main/store.js
@@ -88,8 +88,26 @@ function data (state = defaultDataMap(), action) {
           if (note.isTrashed) {
             state.trashedSet.add(uniqueKey)
             state.starredSet.delete(uniqueKey)
+
+            note.tags.forEach(tag => {
+              let tagNoteList = state.tagNoteMap.get(tag)
+              if (tagNoteList != null) {
+                tagNoteList = new Set(tagNoteList)
+                tagNoteList.delete(uniqueKey)
+                state.tagNoteMap.set(tag, tagNoteList)
+              }
+            })
           } else {
             state.trashedSet.delete(uniqueKey)
+
+            note.tags.forEach(tag => {
+              let tagNoteList = state.tagNoteMap.get(tag)
+              if (tagNoteList != null) {
+                tagNoteList = new Set(tagNoteList)
+                tagNoteList.add(uniqueKey)
+                state.tagNoteMap.set(tag, tagNoteList)
+              }
+            })
 
             if (note.isStarred) {
               state.starredSet.add(uniqueKey)
