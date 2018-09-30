002e7ab9ddaf2db8ac6306a082c8269e790b647b
jacobherrington <jherrington.personal@gmail.com>
2018-09-30 18:26:36 -0500

    Fix bug caused by poor regex

--- browser/main/lib/ConfigManager.js
+++ browser/main/lib/ConfigManager.js
@@ -24,7 +24,7 @@ export const DEFAULT_CONFIG = {
   amaEnabled: true,
   hotkey: {
     toggleMain: OSX ? 'Command + Alt + L' : 'Super + Alt + E',
-    toggleMode: OSX ? 'Command + Option + M' : 'Ctrl + M'
+    toggleMode: OSX ? 'Command + Opt + M' : 'Ctrl + M'
   },
   ui: {
     language: 'en',
@@ -195,7 +195,7 @@ function rewriteHotkey (config) {
   const keys = [...Object.keys(config.hotkey)]
   keys.forEach(key => {
     config.hotkey[key] = config.hotkey[key].replace(/Cmd/g, 'Command')
-    config.hotkey[key] = config.hotkey[key].replace(/Opt/g, 'Alt')
+    config.hotkey[key] = config.hotkey[key].replace(/Opt\s/g, 'Option ')
   })
   return config
 }
