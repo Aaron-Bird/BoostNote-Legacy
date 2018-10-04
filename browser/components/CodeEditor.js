64abd564b4d939f7b1847345893e561706b2d650
Ryan Scott <rscott@gmail.com>
2018-10-03 21:18:25 -0500

    Fix Cmd+Left to go to end of line on Mac

--- browser/components/CodeEditor.js
+++ browser/components/CodeEditor.js
@@ -177,6 +177,9 @@ export default class CodeEditor extends React.Component {
           }
         }
       },
+      'Cmd-Left': function (cm) {
+        cm.execCommand('goLineLeft')
+      },
       'Cmd-T': function (cm) {
         // Do nothing
       },
