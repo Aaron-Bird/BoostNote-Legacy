07aa775de173d62915187ee280d7595872e24239
JianXu <issuehunt@gmail.com>
2018-11-05 00:39:27 +0800

    Add vscode debug support

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,14 @@ var config = Object.assign({}, skeleton, {
     publicPath: 'http://localhost:8080/assets/'
   },
   debug: true,
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    port: 8080,
+    hot: true,
+    inline: true,
+    quiet: true,
+    publicPath: 'http://localhost:8080/assets/'
+  }
 })
 
 module.exports = config
