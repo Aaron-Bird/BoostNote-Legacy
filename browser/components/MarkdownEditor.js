f2a0f59b0804289363b35b70939fb0e1d2ad0074
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-12-13 13:27:20 +0000

    Fixed error on call to bind.

--- browser/components/MarkdownEditor.js
+++ browser/components/MarkdownEditor.js
@@ -276,7 +276,7 @@ class MarkdownEditor extends React.Component {
           fetchUrlTitle={config.editor.fetchUrlTitle}
           enableTableEditor={config.editor.enableTableEditor}
           linesHighlighted={linesHighlighted}
-          onChange={(e) => this.handleChange(e).bind(this)}
+          onChange={(e) => this.handleChange.bind(this)(e)}
           onBlur={(e) => this.handleBlur(e)}
           spellCheck={config.editor.spellcheck}
         />
