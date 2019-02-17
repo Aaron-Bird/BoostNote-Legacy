ed0b370d495b53981ee78147890d7fda59038f69
David Dreher <dredav@users.noreply.github.com>
2019-02-17 13:23:19 +0100

    Change paragraph headlines at the crowdfunding tab

--- browser/main/modals/PreferencesModal/Crowdfunding.js
+++ browser/main/modals/PreferencesModal/Crowdfunding.js
@@ -27,11 +27,11 @@ class Crowdfunding extends React.Component {
         <br />
         <p>{i18n.__('We launched IssueHunt which is an issue-based crowdfunding / sourcing platform for open source projects.')}</p>
         <p>{i18n.__('Anyone can put a bounty on not only a bug but also on OSS feature requests listed on IssueHunt. Collected funds will be distributed to project owners and contributors.')}</p>
-        <div styleName='group-header2--sub'>{i18n.__('### Sustainable Open Source Ecosystem')}</div>
+        <div styleName='group-header2--sub'>{i18n.__('Sustainable Open Source Ecosystem')}</div>
         <p>{i18n.__('We discussed about open-source ecosystem and IssueHunt concept with the Boostnote team repeatedly. We actually also discussed with Matz who father of Ruby.')}</p>
         <p>{i18n.__('The original reason why we made IssueHunt was to reward our contributors of Boostnote project. We’ve got tons of Github stars and hundred of contributors in two years.')}</p>
         <p>{i18n.__('We thought that it will be nice if we can pay reward for our contributors.')}</p>
-        <div styleName='group-header2--sub'>{i18n.__('### We believe Meritocracy')}</div>
+        <div styleName='group-header2--sub'>{i18n.__('We believe Meritocracy')}</div>
         <p>{i18n.__('We think developers who have skills and do great things must be rewarded properly.')}</p>
         <p>{i18n.__('OSS projects are used in everywhere on the internet, but no matter how they great, most of owners of those projects need to have another job to sustain their living.')}</p>
         <p>{i18n.__('It sometimes looks like exploitation.')}</p>
