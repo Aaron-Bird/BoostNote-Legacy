038b87cf709733801727ac6eb792095242faf919
Abner Soares Alves Junior <abner.alves@codeminer42.com>
2019-02-28 15:59:15 -0300

    Fix test moving from Ava to JEST

--- tests/lib/utils.test.js
+++ tests/lib/utils.test.js
@@ -1,14 +1,15 @@
-import test from 'ava'
 import { isMarkdownTitleURL } from '../../browser/lib/utils'
 
-test('isMarkdownTitleURL returns true for valid Markdown title with url', (t) => {
-  t.true(isMarkdownTitleURL('# https://validurl.com'))
-  t.true(isMarkdownTitleURL('## https://validurl.com'))
-  t.true(isMarkdownTitleURL('###### https://validurl.com'))
-})
+describe('isMarkdownTitleURL', () => {
+  it('returns true for valid Markdown title with url', () => {
+    expect(isMarkdownTitleURL('# https://validurl.com')).toBe(true)
+    expect(isMarkdownTitleURL('## https://validurl.com')).toBe(true)
+    expect(isMarkdownTitleURL('###### https://validurl.com')).toBe(true)
+  })
 
-test('isMarkdownTitleURL returns true for invalid Markdown title with url', (t) => {
-  t.false(isMarkdownTitleURL('1 https://validurl.com'))
-  t.false(isMarkdownTitleURL('24 https://validurl.com'))
-  t.false(isMarkdownTitleURL('####### https://validurl.com'))
+  it('returns true for invalid Markdown title with url', () => {
+    expect(isMarkdownTitleURL('1 https://validurl.com')).toBe(false)
+    expect(isMarkdownTitleURL('24 https://validurl.com')).toBe(false)
+    expect(isMarkdownTitleURL('####### https://validurl.com')).toBe(false)
+  })
 })
