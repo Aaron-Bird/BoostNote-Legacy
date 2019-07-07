e940253cafc2d9e065e889ee7b7e92bf8e5252be
Tobias Doll <tobias.doll@gmail.com>
2019-07-07 20:06:50 +0200

    Using single quotes for empty string

--- browser/main/TopBar/index.js
+++ browser/main/TopBar/index.js
@@ -71,7 +71,7 @@ class TopBar extends React.Component {
     this.refs.search.childNodes[0].blur
     dispatch(push('/searched'))
     e.preventDefault()
-    this.debouncedUpdateKeyword("")
+    this.debouncedUpdateKeyword('')
   }
 
   handleKeyDown (e) {
