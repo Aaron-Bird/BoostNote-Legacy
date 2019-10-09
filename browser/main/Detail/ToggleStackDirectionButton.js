1993a6588da298816589bead2e2839502dd55987
Callum Booth <callum-booth@live.co.uk>
2019-10-09 12:45:52 +0100

    Cleans up toggle button component

--- browser/main/Detail/ToggleStackDirectionButton.js
+++ browser/main/Detail/ToggleStackDirectionButton.js
@@ -6,15 +6,16 @@ import i18n from 'browser/lib/i18n'
 
 const ToggleStackDirectionButton = ({
   onClick, isStacking
-}) => (
-  <button styleName='control-splitPanelDirection' onClick={() => onClick(!isStacking)}>
-    <img styleName='iconInfo' src={isStacking ? '../resources/icon/icon-panel-split-vertical.svg' : '../resources/icon/icon-panel-split-horizontal.svg'} />
-    <span lang={i18n.locale} styleName='tooltip'>{
-          isStacking ? i18n.__('Split Panels Horizontally') : i18n.__('Split Panels Vertically')
-
-          }</span>
-  </button>
-)
+}) => {
+  const imgSrc = isStacking ? '../resources/icon/icon-panel-split-vertical.svg' : '../resources/icon/icon-panel-split-horizontal.svg'
+  const text = isStacking ? i18n.__('Split Panels Horizontally') : i18n.__('Split Panels Vertically')
+  return (
+    <button styleName='control-splitPanelDirection' onClick={() => onClick(!isStacking)}>
+      <img styleName='iconInfo' src={imgSrc} />
+      <span lang={i18n.locale} styleName='tooltip'>{text}</span>
+    </button>
+  )
+}
 
 ToggleStackDirectionButton.propTypes = {
   onClick: PropTypes.func.isRequired,
